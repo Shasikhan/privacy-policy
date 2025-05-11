60b3385e2e1a60f8fd0e5967ceb80cd457c0a162
Shafqat Ullah Khan <32072222+Shasikhan@users.noreply.github.com>
2025-05-11 21:59:04 +0500

    initizalized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'Deploy your static Next.js site to GitHub Pages.',
+  title: "Privacy Policies",
+  description: "Privacy Policies for Applications",
 };
 
 export default function RootLayout({
