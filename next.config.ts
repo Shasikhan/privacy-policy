35ea957fb46782ca869f62c6327e58f920ca3a58
Shafqat Ullah Khan <32072222+Shasikhan@users.noreply.github.com>
2025-05-12 11:56:44 +0500

    designed main page

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
+const isGithubPages = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/privacy-policy",
+  basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: isGithubPages ? "/privacy-policy/" : "",
   images: { unoptimized: true },
 };
 
